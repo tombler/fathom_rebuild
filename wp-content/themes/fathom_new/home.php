82f8f93d1827311198d952844ce16a8afb87c0ad
Tom Griffey <tgriffey9389@gmail.com>
2016-08-09 21:25:15 -0500

    New content for home page.

--- wp-content/themes/fathom_new/home.php
+++ wp-content/themes/fathom_new/home.php
@@ -5,7 +5,7 @@
       <div class="section bg-1" id="section-header">
         <div id="hero-content" class="container">
           <div class="row text-center">
-            <h1>EMPOWER. INNOVATE. SUSTAIN.</h1>
+            <h1>Create the career you care about.</h1>
             <h3>Fathom is a social enterprise for young people to earn a living pursuing their passion.</h3>
             <div class="col-md-2 col-md-offset-5">
               <a href="#section-about" class="btn btn-lg text-center feature-btn hvr-shutter-out-horizontal">LEARN MORE</a>  
@@ -14,20 +14,96 @@
         </div>
       </div> <!-- section -->
 
-      <div class="section" id="section-about">
+      <div class="section bg-3" id="section-about">
         <div class="container">
           <div class="row featurette">
-            <div class="col-md-12">
-              <img class="img-responsive center-block" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/fathom-your-reality-seed-grey-01.svg" alt="Fathom" width="100px">
-              <h2 class="section-header text-center">ABOUT FATHOM</h2>
+            <div class="col-md-4">
+              <img class="img-responsive center-block" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/fathom-your-reality-seed-grey-01.svg" alt="Fathom" width="200px">
+            </div>
+            <div class="col-md-8">
+              <h2 class="section-header text-left">FATHOM</h2>
               <hr>
-              <p>
-                Fathom is the Social Network for Social Change. As a lifestyle platform for youth empowerment, we envision a reality in which the youth and their communities are empowered as proactive agents of change. Our mission is to cultivate and incentivize collaborative creativity, social engagement and entrepreneurial solutions that support the emergence of conscious communities, companies and commodities. In essence, Fathom is an idea… your idea and a way to share it with those around you. We empower our generation to pursue their passion and forge their future, while providing assistance to seed ideas from concept to creation. We challenge you to Fathom your reality.
+              <h3 class="text-left">
+                <!-- Fathom is the Social Network for Social Change. As a lifestyle platform for youth empowerment, we envision a reality in which the youth and their communities are empowered as proactive agents of change. Our mission is to cultivate and incentivize collaborative creativity, social engagement and entrepreneurial solutions that support the emergence of conscious communities, companies and commodities. In essence, Fathom is an idea… your idea and a way to share it with those around you. We empower our generation to pursue their passion and forge their future, while providing assistance to seed ideas from concept to creation. We challenge you to Fathom your reality. -->
+                Fathom provides Online and real-world tools to leverage entrepreneurial problem-solving for social change. We connect and reward youth and their communities to:<br><br>
+              </h3>
+              <ul class="text-left">
+                <li><h3><strong>Cultivate Lifelong Learning & Creativity</strong></h3></li>
+                <li><h3><strong>Create Careers & Broker Opportunities</strong></h3></li>
+                <li><h3><strong>Reimagine Local Economies for the 21st Century</strong></h3></li>
+              </ul>
+            </div>
+          </div> <!-- row -->
+        </div> <!-- container -->
+      </div> <!-- section -->
+
+      <div class="section bg-2" id="section-creatives">
+        <div class="container">
+          <div class="row featurette">
+            <div class="col-md-8">
+              <h2 class="section-header text-left">CREATIVES</h2>
+              <hr>
+              <h3>Earn a Living Pursuing Your Passion</h3>
+              <br><br>
+              <p class="text-left">
+                Work should be more than just earning a paycheck.
+It should be doing what you love and loving what
+you do.</p><p>
+Learn more how Fathom empowers young people with
+tools to create careers they care about.  
+              </p>
+            </div>
+            <div class="col-md-4">
+              <img class="img-responsive center-block" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/fathom-your-reality-seed-01.svg" alt="Fathom" width="200px">
+            </div>
+          </div> <!-- row -->
+        </div> <!-- container -->
+      </div> <!-- section -->
+
+
+      <div class="section bg-3" id="section-app">
+        <div class="container">
+          <div class="row featurette">
+            <div class="col-md-4">
+              <img class="img-responsive center-block" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/fathom-your-reality-seed-grey-01.svg" alt="Fathom" width="200px">
+            </div>
+            <div class="col-md-8">
+              <h2 class="section-header text-left">COMMUNITY PARTNERS</h2>
+              <hr>
+              <h3>To Invest in the Youth is to Invest in the Future</h3>
+              <br><br>
+              <p class="text-left">
+                To Invest in the Youth is to Invest in the Future
+Schools, businesses and organizations have the
+power to seed the investment for a thriving youth. </p>
+<p> 
+Learn more how Fathom supports education, 
+small business and workforce development for the
+21st century economy. 
               </p>
             </div>
           </div> <!-- row -->
         </div> <!-- container -->
       </div> <!-- section -->
+
+      <!-- <div class="section bg-3" id="section-app">
+        <div id="app-wrapper">
+        <div class="container">
+          <div class="row featurette">
+            <div class="col-md-5">
+              <img class="img-responsive center-block" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/thefathom-app.png" alt="Fathom App" width="450px">
+            </div>
+            <div class="col-md-7">
+              <h2>FATHOM APP</h2>
+              <hr>
+              <p>
+                Fathom’s mobile app empowers users to create and discover meaningful opportunities via entrepreneurial problem-solving. Do you have the next great idea that can change the world or make it a better place? If so, Fathom’s mobile app is for you. We're now inviting Nashville students, creatives and community partners  for Beta testing.  </br></br><a href="/contact" class="link">Contact us</a> to recieve a special access code on our Beta release date.
+              </p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div> -->
 
       <div class="section bg-2" id="section-projects">
         <div class="container" id="projects-header">
@@ -76,23 +152,6 @@
         </div> <!-- container --> 
       </div> <!-- section -->
 
-      <div class="section bg-3" id="section-app">
-        <div id="app-wrapper">
-        <div class="container">
-          <div class="row featurette">
-            <div class="col-md-5">
-              <img class="img-responsive center-block" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/thefathom-app.png" alt="Fathom App" width="450px">
-            </div> <!-- col -->
-            <div class="col-md-7">
-              <h2 class="section-header text-center">FATHOM APP</h2>
-              <p>
-                Fathom’s mobile app empowers users to create and discover meaningful opportunities via entrepreneurial problem-solving. Do you have the next great idea that can change the world or make it a better place? If so, Fathom’s mobile app is for you. We're now inviting Nashville students, creatives and community partners  for Beta testing.  </br></br><a href="/contact" class="link">Contact us</a> to recieve a special access code on our Beta release date.
-              </p>
-            </div> <!-- col -->
-          </div> <!-- row -->
-        </div> <!-- container -->
-        </div>
-      </div> <!-- section -->
 
     </div>
 
--- wp-content/themes/fathom_new/template-contact.php
+++ wp-content/themes/fathom_new/template-contact.php
@@ -8,9 +8,16 @@ Template Name: Contact
 <?php include('header.php'); ?> 
 
 <div id="fullpage">
-    <div class="section">
-      <h1 class="text-center">CONTACT</h1>
-      <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 1 ); } ?> 
+    <div class="section individual-project">
+        <div class="container">
+            <div class="row short-form">
+                <div class="col-md-6 col-md-offset-3">
+                    <h2 class="text-center">CONTACT</h2>
+                    <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 1 ); } ?> 
+                </div>
+            </div>
+        </div> 
+      
     </div> <!-- section -->
 </div> <!-- fullpage -->
 
